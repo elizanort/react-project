293d59892f313c8241cd9ff382525eeb36bc52a8
“elizanort” <elizabethnortman@gmail.com>
2020-05-15 16:00:00 -0400

    created a scss file and finished starting a react project - activity

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 import React from 'react'
-import './App.css';
+import './App.scss';
 
 
 function App() {
   return (
     <body>
-      <nav className="sidenav">
-          <div className="topnav_container">
+      <div className="page_container">
+
+      <nav className="sidenav_container">
+          <div className="topnav">
             <img className="profile_img"></img>
             <h3 className="username_link"></h3>
           </div>
-          <div className="todolists_container">
-            <a href="">ToDo</a>
-            <a href="">Grocery List</a>
-            <a href="">Contacts</a>
+
+          <div  className="todolists">
+            <a href="#">ToDo</a>
+            <a href="#">Grocery List</a>
+            <a href="#">Contacts</a>
           </div>
-          <div className="bottomnav_container">
-            <a href="">New List</a>
+
+          <div className="bottomnav">
+            <a href="#">New List</a>
             <img src=""></img>
           </div>
       </nav>
-      <div className="list_container">
+
+      <div className="mainpage_container">
         <div className="header">
           <h1>ToDo List</h1>
           <a href=""></a>
         </div>
-        <div>
+
+        <div className="listitems">
           <ul>
             <li>Work Out</li>
             <li>Write Letter to Olivia</li>
           </ul>
         </div>
-
       </div>
-      
-    <div className ="too">
-    <ul>
-      <li>Go to the movies</li>
-      <li>Finish homework</li>
-    </ul>
+
     </div>
   </body>
   );
