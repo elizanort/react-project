6b1619c0f18b1d42924e20f97c8e147a32d03cd5
“elizanort” <elizabethnortman@gmail.com>
2020-05-17 15:52:09 -0400

    added styling & positioning with css. cannot get otf font to work

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
-import React from 'react'
-import './App.scss';
-
+import React from "react";
+import "./App.scss";
+import profileimg from "./imgs/profile.svg";
 
 function App() {
   return (
-    <body>
-      <div className="page_container">
+    <body className="page_container">
+      <div className="sidebar_container">
+        <div className="logo">
+          <h1>CHECKCHECK</h1>
+        </div>
+        <div className="solidline"></div>
+        <div className="solidline"></div>
+        <div className="solidline"></div>
 
-      <nav className="sidenav_container">
-          <div className="topnav">
-            <img className="profile_img"></img>
-            <h3 className="username_link"></h3>
-          </div>
-
-          <div  className="todolists">
-            <a href="#">ToDo</a>
-            <a href="#">Grocery List</a>
-            <a href="#">Contacts</a>
-          </div>
-
-          <div className="bottomnav">
-            <a href="#">New List</a>
-            <img src=""></img>
-          </div>
-      </nav>
-
-      <div className="mainpage_container">
-        <div className="header">
-          <h1>ToDo List</h1>
-          <a href=""></a>
+        <div className="todo">
+          <a href="www.google.com" className="todo_buttons">
+            ToDo
+          </a>
+          <a href="www.google.com" className="todo_buttons">
+            Grocery List
+          </a>
+          <a href="www.google.com" className="todo_buttons">
+            Contacts
+          </a>
+          <a href="www.google.com" className="todo_buttons">
+            New List
+          </a>
         </div>
 
-        <div className="listitems">
-          <ul>
-            <li>Work Out</li>
-            <li>Write Letter to Olivia</li>
-          </ul>
+        <div className="bottom">
+          <img src={profileimg} className="profile_img" alt="profile"></img>
+          <a href="www.google.com" className="username_link">
+            Elizabeth Nortman{" "}
+          </a>
         </div>
       </div>
 
-    </div>
-  </body>
+      <div className="main_container">
+        <div className="header">
+          <h1 className="header_h1">ToDo List</h1>
+        </div>
+
+        <div className="todolist">
+          <ul>
+            <li className="todolist_listitems">
+              <input type="checkbox"></input> 
+              <p>Work Out</p>
+            </li>
+            <li className="todolist_listitems">
+              <input type="checkbox"></input>
+              <p>Write Letter to Olivia</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </body>
   );
 }
-
 
 export default App;
